9b52bae2016c1198ee70e0f4dca182a1d3a68da8
NuclearRedeye <j.summerfield@elifesciences.org>
2020-04-28 16:13:12 +0100

    chore: add missing drop down arrow

--- src/app/components/drop-down-menu/index.tsx
+++ src/app/components/drop-down-menu/index.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 export interface DropDownMenuItemProps {
   title: string;
@@ -46,6 +46,7 @@ export const DropDownMenu: React.FC<DropDownMenuProps> = ({ title, entries }) =>
     <div>
       <Button aria-controls='simple-menu' aria-haspopup='true' onClick={handleMenuClick}>
         {title}
+        <ArrowDropDownIcon />
       </Button>
       <Menu id='simple-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleMenuClose}>
         {items}
