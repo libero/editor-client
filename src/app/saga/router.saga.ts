4a52bbca15841d4dce13f8f7455bcc4b8035fdfd
NuclearRedeye <j.summerfield@elifesciences.org>
2020-05-13 15:44:40 +0100

    fix: break out url processing into seperate function and add some docs

--- src/app/saga/router.saga.ts
+++ src/app/saga/router.saga.ts
@@ -1,18 +1,28 @@
 import { all, takeLatest, put } from 'redux-saga/effects';
 import * as initActions from '../actions/init.actions';
 import { LOCATION_CHANGE, LocationChangeAction } from 'connected-react-router';
+import { getKeyFromQueryParams } from '../utils/url.utils';
 
-const articleIdRegex = /A?articleId=[^&]+&*/;
-
+/**
+ * Side effect handler for a location change event that will, if present, start to load the specified article.
+ *
+ * @export
+ * @param {LocationChangeAction} action
+ */
 export function* routerLocationChanged(action: LocationChangeAction) {
-  const found = action.payload.location.search.match(articleIdRegex);
-  if (found) {
-    yield put(initActions.loadArticle(found[0].split('=')[1]));
+  const articleId = getKeyFromQueryParams(action.payload.location.search, 'articleId');
+  if (articleId) {
+    yield put(initActions.loadArticle(articleId));
   } else if (action.payload.isFirstRendering) {
     yield put(initActions.loadArticle('00104'));
   }
 }
 
+/**
+ * A redux-saga that binds location change events to the handler function.
+ *
+ * @export
+ */
 export function* routerSaga() {
   yield all([takeLatest(LOCATION_CHANGE, routerLocationChanged)]);
 }
