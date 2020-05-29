61a89407c6ff6737f130cc49cfd2a38d19bf22a9
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-05-29 14:33:08 +0100

    task: additional styles fixed (#36)

--- src/app/components/prompt-dialog/styles.ts
+++ src/app/components/prompt-dialog/styles.ts
@@ -1,8 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const usePromptDialogStyles = makeStyles((theme) => ({
+  dialogTitle: {
+    padding: theme.spacing(3, 3, 1, 3)
+  },
   actionPanel: {
-    marginBottom: theme.spacing(2),
-    marginRight: theme.spacing(3)
+    padding: 0,
+    marginBottom: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    '& > *:last-child': {
+      marginLeft: theme.spacing(2)
+    }
   }
 }));
