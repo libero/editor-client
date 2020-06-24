2ce49776b3eafe614b79cb33b9132cb853e9399b
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-06-24 11:38:00 +0100

    chore: tests updated

--- src/app/containers/manuscript/references-list/__tests__/references-list.spec.tsx
+++ src/app/containers/manuscript/references-list/__tests__/references-list.spec.tsx
@@ -1,11 +1,64 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import { create } from 'react-test-renderer';
+import { EditorState } from 'prosemirror-state';
 import { Provider } from 'react-redux';
 
 import { ReferenceList } from 'app/containers/manuscript/references-list/index';
 import { givenState } from 'app/test-utils/reducer-test-helpers';
-import ReferenceData from './reference-data.mock';
+import { createReferenceAnnotatedValue, Reference } from 'app/models/reference';
+
+const ReferenceData = [
+  {
+    id: 'bib1',
+    authors: [
+      {
+        firstName: 'V',
+        lastName: 'Berk'
+      }
+    ],
+    type: 'journal',
+    referenceInfo: {
+      year: 2012,
+      source: stringToEditorState('Science'),
+      articleTitle: stringToEditorState(
+        'Molecular architecture and assembly principles of <italic>Vibrio cholerae</italic> biofilms'
+      ),
+      doi: '',
+      pmid: '',
+      elocationId: '',
+      firstPage: 236,
+      lastPage: 239,
+      inPress: false,
+      volume: 337
+    }
+  },
+  {
+    id: 'bib2',
+    authors: [
+      {
+        firstName: 'PK',
+        lastName: 'Feyerabend'
+      }
+    ],
+    type: 'book',
+    referenceInfo: {
+      year: 2010,
+      source: stringToEditorState('The skull and brain'),
+      chapterTitle: stringToEditorState('The skull and brain'),
+      publisherLocation: 'London',
+      publisherName: 'Verso',
+      edition: '4th Edition',
+      doi: '',
+      pmid: '',
+      elocationId: '',
+      firstPage: 0,
+      lastPage: 0,
+      inPress: false,
+      volume: 0
+    }
+  }
+] as Reference[];
 
 jest.mock('@material-ui/core', () => ({
   Button: ({ label }) => <div data-cmp="Button">{label}</div>,
@@ -26,3 +79,9 @@ describe('References List', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
+
+function stringToEditorState(xmlContent: string): EditorState {
+  const el = document.createElement('div');
+  el.innerHTML = xmlContent;
+  return createReferenceAnnotatedValue(el);
+}
