18590d9b69ab2c73ec4bbb9aa9bf93f3fd8f1aad
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-08-04 09:52:07 +0100

    fix: pr feedback

--- src/app/containers/reference-form-dialog/referenc-forms.config.ts
+++ src/app/containers/reference-form-dialog/referenc-forms.config.ts
@@ -21,11 +21,11 @@ export const JOURNAL_FORM: Record<string, FormControlConfigType> = {
 export const PERIODICAL_FORM: Record<string, FormControlConfigType> = {
   date: { type: 'date', label: 'Date' },
   articleTitle: { type: 'rich-text', label: 'Title' },
-  source: { type: 'rich-text', label: 'Journal title' },
+  source: { type: 'rich-text', label: 'Periodical title' },
   volume: { type: 'number', label: 'Volume' },
   firstPage: { type: 'string', label: 'First page' },
   lastPage: { type: 'string', label: 'Last page' },
-  extLink: { type: 'string', label: 'ext link' }
+  extLink: { type: 'string', label: 'URL' }
 };
 
 export const BOOK_FORM: Record<string, FormControlConfigType> = {
@@ -50,7 +50,7 @@ export const REPORT_FORM: Record<string, FormControlConfigType> = {
   source: { type: 'rich-text', label: 'Title' },
   publisherName: { type: 'string', label: 'Publisher Name' },
   doi: { type: 'string', label: 'DOI' },
-  extLink: { type: 'string', label: 'ext link' }
+  extLink: { type: 'string', label: 'URL' }
 };
 
 export const DATA_FORM: Record<string, FormControlConfigType> = {
@@ -61,7 +61,7 @@ export const DATA_FORM: Record<string, FormControlConfigType> = {
   doi: { type: 'string', label: 'DOI' },
   accessionId: { type: 'string', label: 'Accession' },
   accessionUrl: { type: 'string', label: 'Accession URL' },
-  extLink: { type: 'string', label: 'ext link' },
+  extLink: { type: 'string', label: 'URL' },
   specificUse: { type: 'string', label: 'Specific Use' }
 };
 
@@ -70,7 +70,7 @@ export const WEB_FORM: Record<string, FormControlConfigType> = {
   year: { type: 'number', label: 'Year' },
   articleTitle: { type: 'rich-text', label: 'Article title' },
   source: { type: 'rich-text', label: 'Source' },
-  extLink: { type: 'string', label: 'ext link' }
+  extLink: { type: 'string', label: 'URL' }
 };
 
 export const PREPRINT_FORM: Record<string, FormControlConfigType> = {
@@ -80,7 +80,7 @@ export const PREPRINT_FORM: Record<string, FormControlConfigType> = {
   source: { type: 'rich-text', label: 'Journal title' },
   doi: { type: 'string', label: 'DOI' },
   pmid: { type: 'string', label: 'PMID' },
-  extLink: { type: 'string', label: 'ext link' }
+  extLink: { type: 'string', label: 'URL' }
 };
 
 export const SOFTWARE_FORM: Record<string, FormControlConfigType> = {
