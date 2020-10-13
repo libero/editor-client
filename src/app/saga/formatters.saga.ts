83752701a9a27d00c4a375e04d9031dacd896a5e
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-10-13 08:43:07 +0100

    fix box insertion issue

--- src/app/saga/formatters.saga.ts
+++ src/app/saga/formatters.saga.ts
@@ -23,12 +23,17 @@ async function insertBox(editorState: EditorState): Promise<Transaction> {
       : editorState.schema.nodes.paragraph.createAndFill(null, editorState.schema.text(' '));
 
   const change = await new Promise<Transaction>((resolve) => {
-    splitBlockKeepMarks(editorState, (tr: Transaction) => resolve(tr));
+    // only split paragraph if it has content
+    if (editorState.selection.$from.parent.textContent.trim()) {
+      splitBlockKeepMarks(editorState, (tr: Transaction) => resolve(tr));
+    } else {
+      resolve(editorState.tr);
+    }
   });
 
   const box = editorState.schema.nodes['boxText'].createAndFill(null, content);
-  if (!change.selection.$from.parent.textContent) {
-    change.setSelection(NodeSelection.create(change.doc, $from.after())).replaceSelectionWith(box);
+  if (!change.selection.$from.parent.textContent.trim()) {
+    change.setSelection(NodeSelection.create(change.doc, change.selection.$from.pos - 1)).replaceSelectionWith(box);
   } else {
     change.insert(change.selection.$from.pos - 1, box);
   }
