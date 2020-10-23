9662795f94d2c5d6331f1e93e23ebaa447a3c2e3
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-10-23 11:24:40 +0100

    task: address design feedback

--- src/app/components/figure/styles.ts
+++ src/app/components/figure/styles.ts
@@ -4,20 +4,19 @@ export const useFigureEditorStyles = makeStyles((theme) => ({
   figureContainer: {
     display: 'flex',
     alignItems: 'flex-start',
-    '& label': {
-      backgroundColor: '#FAFAFA'
-    },
+
     '& .MuiOutlinedInput-root.Mui-focused fieldset': {
       borderWidth: 1
     }
   },
   figureContent: {
     flexGrow: 1,
-    padding: theme.spacing(2),
+    padding: theme.spacing(3, 2, 2, 2),
     margin: theme.spacing(5, 0),
-    borderRadius: theme.spacing(0.5),
-    backgroundColor: '#FAFAFA',
-    boxShadow: '0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.2)'
+    borderRadius: theme.shape.borderRadius,
+    borderColor: 'rgba(0, 0, 0, 0.23)',
+    borderStyle: 'solid',
+    borderWidth: '1px'
   },
   deleteButton: {
     flexGrow: 0,
@@ -35,9 +34,19 @@ export const useFigureEditorStyles = makeStyles((theme) => ({
   uploadImageCta: {
     position: 'absolute',
     top: theme.spacing(1),
-    right: theme.spacing(1)
+    right: theme.spacing(1),
+    backgroundColor: '#929393',
+    padding: 10,
+    '&:hover': {
+      backgroundColor: '#929393'
+    },
+    '& svg': {
+      fill: '#FFFFFF'
+    }
   },
   image: {
-    maxWidth: '100%'
+    maxWidth: '100%',
+    borderRadius: theme.shape.borderRadius,
+    borderColor: '#C4C4C4'
   }
 }));
