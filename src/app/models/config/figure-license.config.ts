852fd3e1c451757b8e37b6c140fe37f090d917d6
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-10-26 16:04:36 +0000

    task: parse asset-level licensing (#127)

---
+++ src/app/models/config/figure-license.config.ts
@@ -0,0 +1,3 @@
+export const marks = ['italic', 'bold', 'subscript', 'superscript', 'strikethrough', 'underline', 'link'];
+export const nodes = ['doc', 'text', 'refCitation', 'paragraph'];
+export const topNode = 'doc';
