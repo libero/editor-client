d9a166b57061dbe92cd8055129458d2716f557fb
noris alsaadie <>
2020-10-29 19:15:18 +0000

    remove local stack

--- .localstack/startup/create-sqs.sh
+++
@@ -1,6 +0,0 @@
-echo 'creating SQS queue'
-awslocal sqs create-queue --queue-name KryiaUploadQueue
-echo 'SQS queue created'
-echo 'setting up bucket notifications'
-awslocal s3api put-bucket-notification-configuration --bucket kryia --notification-configuration file:///etc/localstackconf/notification.json
-echo 'bucket notifications ready'
