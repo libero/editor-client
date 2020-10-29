c23d489b9bd0322ace5c81b6e2f447437093d779
noris alsaadie <>
2020-10-29 19:20:00 +0000

    Restore formatting and include change

--- src/app/api/manuscript.api.ts
+++ src/app/api/manuscript.api.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { Manuscript } from "app/models/manuscript";
+import axios from 'axios';
+import { Manuscript } from 'app/models/manuscript';
 import {
   createTitleState,
   createKeywordGroupsState,
@@ -7,36 +7,34 @@ import {
   createReferencesState,
   createImpactStatementState,
   createAcknowledgementsState,
-  createBodyState,
-} from "app/models/manuscript-state.factory";
-import { createAuthorsState } from "app/models/person";
-import { createAffiliationsState } from "app/models/affiliation";
-import { getTextContentFromPath } from "app/models/utils";
-import { createRelatedArticleState } from "app/models/related-article";
-import { createArticleInfoState } from "app/models/article-information";
+  createBodyState
+} from 'app/models/manuscript-state.factory';
+import { createAuthorsState } from 'app/models/person';
+import { createAffiliationsState } from 'app/models/affiliation';
+import { getTextContentFromPath } from 'app/models/utils';
+import { createRelatedArticleState } from 'app/models/related-article';
+import { createArticleInfoState } from 'app/models/article-information';
 
 const manuscriptUrl = (id: string): string => {
-  return `/api/v1/articles/${id}`;
+  return `/api/v1/articles/${id}/`;
 };
 
 export async function getManuscriptContent(id: string): Promise<Manuscript> {
-  const { data } = await axios.get<string>(manuscriptUrl(id), {
-    headers: { Accept: "application/xml" },
-  });
+  const { data } = await axios.get<string>(manuscriptUrl(id), { headers: { Accept: 'application/xml' } });
 
   const parser = new DOMParser();
-  const doc = parser.parseFromString(data, "text/xml");
-  const title = doc.querySelector("title-group article-title");
-  const keywordGroups = doc.querySelectorAll("kwd-group");
-  const abstract = doc.querySelector("abstract:not([abstract-type])");
+  const doc = parser.parseFromString(data, 'text/xml');
+  const title = doc.querySelector('title-group article-title');
+  const keywordGroups = doc.querySelectorAll('kwd-group');
+  const abstract = doc.querySelector('abstract:not([abstract-type])');
   const impactStatement = doc.querySelector('abstract[abstract-type="toc"]');
   const authors = doc.querySelectorAll('contrib[contrib-type="author"]');
-  const affiliations = doc.querySelectorAll("contrib-group:first-of-type aff");
-  const references = doc.querySelectorAll("ref-list ref element-citation");
-  const authorNotes = doc.querySelector("author-notes");
-  const relatedArticles = doc.querySelectorAll("related-article");
-  const acknowledgements = doc.querySelector("ack");
-  const body = doc.querySelector("body");
+  const affiliations = doc.querySelectorAll('contrib-group:first-of-type aff');
+  const references = doc.querySelectorAll('ref-list ref element-citation');
+  const authorNotes = doc.querySelector('author-notes');
+  const relatedArticles = doc.querySelectorAll('related-article');
+  const acknowledgements = doc.querySelector('ack');
+  const body = doc.querySelector('body');
 
   const authorsState = createAuthorsState(Array.from(authors), authorNotes);
 
@@ -46,18 +44,15 @@ export async function getManuscriptContent(id: string): Promise<Manuscript> {
     impactStatement: createImpactStatementState(impactStatement),
     keywordGroups: createKeywordGroupsState(Array.from(keywordGroups)),
     authors: authorsState,
-    body: createBodyState(body, id),
+    body: createBodyState(body),
     affiliations: createAffiliationsState(Array.from(affiliations)),
     references: createReferencesState(Array.from(references)),
     relatedArticles: createRelatedArticleState(Array.from(relatedArticles)),
     acknowledgements: createAcknowledgementsState(acknowledgements),
     articleInfo: createArticleInfoState(doc, authorsState),
     journalMeta: {
-      publisherName: getTextContentFromPath(
-        doc,
-        "journal-meta publisher publisher-name"
-      ),
-      issn: getTextContentFromPath(doc, "journal-meta issn"),
-    },
+      publisherName: getTextContentFromPath(doc, 'journal-meta publisher publisher-name'),
+      issn: getTextContentFromPath(doc, 'journal-meta issn')
+    }
   } as Manuscript;
 }
