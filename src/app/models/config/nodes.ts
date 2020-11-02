36ab2bf4bf59fe8d85d6a26663ef9d88d7af13da
noris alsaadie <>
2020-11-02 11:55:51 +0000

    Lint

--- src/app/models/config/nodes.ts
+++ src/app/models/config/nodes.ts
@@ -187,9 +187,14 @@ export const nodes = {
         getAttrs(dom) {
           const paths = get(dom.ownerDocument, 'manuscriptPath').split('/');
           const id = paths[2];
-          const url = process.env.REACT_APP_NO_SERVER 
-            ? get(dom.ownerDocument, 'manuscriptPath') + '/' + get(dom.querySelector('graphic'), 'attributes.xlink:href.value') 
-            : `/api/v1/articles/${id}/assets/${get(dom.querySelector('graphic'), 'attributes.xlink:href.value').replace('tif', 'jpg')}`;
+          const url = process.env.REACT_APP_NO_SERVER
+            ? get(dom.ownerDocument, 'manuscriptPath') +
+              '/' +
+              get(dom.querySelector('graphic'), 'attributes.xlink:href.value')
+            : `/api/v1/articles/${id}/assets/${get(dom.querySelector('graphic'), 'attributes.xlink:href.value').replace(
+                'tif',
+                'jpg'
+              )}`;
           return {
             label: getTextContentFromPath(dom, 'label') || '',
             img: url,
