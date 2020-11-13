e58198bfc72834b2b221d30f68acea6864a91793
Cory <c.donkin@elifesciences.org>
2020-11-13 10:18:43 +0000

    remove post request from onchange

--- src/app/components/rich-text-editor/index.tsx
+++ src/app/components/rich-text-editor/index.tsx
@@ -131,15 +131,6 @@ export class RichTextEditor extends React.Component<ComponentWithId<RichTextEdit
     const newState = this.editorView.state.apply(change);
     this.updateEditorState(newState);
     this.props.onChange(change, this.props.name);
-
-    // this should be moved to redux
-    // const { data } = axios.get<string>(manuscriptUrl(id), { headers: { Accept: 'application/xml' } });
-    const id = '00104';
-    await axios.post(`/api/v1/articles/${id}/changes`, { steps: change.steps, path: 'abstract' })
-
-    // data structure that we need for the backend
-    // [{path: 'title', steps: [...]}, {path: 'keywords', steps: [...]}, {path: 'abstract', steps: [...]}] OR
-    // {'title': {steps: []}, 'keywords': {steps: []}, 'abstract': {steps: []}}
   }
 
   private createEditorView = (element: HTMLElement) => {
