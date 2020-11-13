b9879816a3f0d4720bfe266a3c64c5443eec4178
Cory <c.donkin@elifesciences.org>
2020-11-13 10:25:54 +0000

    remove post request from onChange (#138)
    * remove post request from onchange
    
    * linting fix

--- src/app/components/rich-text-editor/index.tsx
+++ src/app/components/rich-text-editor/index.tsx
@@ -3,8 +3,6 @@ import { EditorView } from 'prosemirror-view';
 import { EditorState, Transaction, TextSelection } from 'prosemirror-state';
 import { debounce, get } from 'lodash';
 import { DOMSerializer, Slice } from 'prosemirror-model';
-import axios from 'axios';
-
 import 'prosemirror-example-setup/style/style.css';
 import 'prosemirror-menu/style/menu.css';
 import { SectionContainer, SectionContainerVariant } from 'app/components/section-container';
@@ -131,15 +129,6 @@ export class RichTextEditor extends React.Component<ComponentWithId<RichTextEdit
     const newState = this.editorView.state.apply(change);
     this.updateEditorState(newState);
     this.props.onChange(change, this.props.name);
-
-    // this should be moved to redux
-    // const { data } = axios.get<string>(manuscriptUrl(id), { headers: { Accept: 'application/xml' } });
-    const id = '00104';
-    await axios.post(`/api/v1/articles/${id}/changes`, { steps: change.steps, path: 'abstract' })
-
-    // data structure that we need for the backend
-    // [{path: 'title', steps: [...]}, {path: 'keywords', steps: [...]}, {path: 'abstract', steps: [...]}] OR
-    // {'title': {steps: []}, 'keywords': {steps: []}, 'abstract': {steps: []}}
   }
 
   private createEditorView = (element: HTMLElement) => {
