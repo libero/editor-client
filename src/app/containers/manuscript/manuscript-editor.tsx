c9607cf30b1acbac825bca0fdfc480eba0ce8625
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-11-27 10:58:44 +0000

    fix build

--- src/app/containers/manuscript/manuscript-editor.tsx
+++ src/app/containers/manuscript/manuscript-editor.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, SyntheticEvent } from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { EditorState, Transaction } from 'prosemirror-state';
 
