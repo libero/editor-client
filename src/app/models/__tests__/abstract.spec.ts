9d0f1cfa667b5510c9b449648dfe51922f98120d
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-12-17 18:17:58 +0000

    more tests

--- src/app/models/__tests__/abstract.spec.ts
+++ src/app/models/__tests__/abstract.spec.ts
@@ -1,10 +1,17 @@
-import { createAbstractState } from 'app/models/abstract';
+import { createAbstractState, createImpactStatementState } from 'app/models/abstract';
 
 describe('Manuscript state factory', () => {
   it('creates abstract state', () => {
     const el = document.createElement('abstract');
     el.innerHTML = 'A sample <italic>text</italic> with <sub>a</sub> variety of <sup>tags</sup>';
     const editorState = createAbstractState(el);
+    expect(editorState).toMatchSnapshot();
+  });
+
+  it('creates impact statement state', () => {
+    const el = document.createElement('abstract');
+    el.innerHTML = 'A sample <italic>text</italic> with <sub>a</sub> variety of <sup>tags</sup>';
+    const editorState = createImpactStatementState(el);
     expect(editorState).toMatchSnapshot();
   });
 });
---
+++ src/app/models/__tests__/body.spec.ts
@@ -0,0 +1,10 @@
+import { createBodyState } from 'app/models/body';
+
+describe('Manuscript state factory', () => {
+  it('creates body state', () => {
+    const el = document.createElement('body');
+    el.innerHTML = 'A sample <italic>text</italic> with <sub>a</sub> variety of <sup>tags</sup>';
+    const editorState = createBodyState(el, 'SOME_ID');
+    expect(editorState).toMatchSnapshot();
+  });
+});
