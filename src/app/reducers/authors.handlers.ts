4637cfda9edf61bd695cfc819b69648530b61990
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-01-07 15:30:47 +0200

    edit authors using commands

--- src/app/reducers/authors.handlers.ts
+++ src/app/reducers/authors.handlers.ts
@@ -4,98 +4,84 @@ import { MoveAuthorPayload } from 'app/actions/manuscript.actions';
 import { RearrangingChange } from 'app/utils/history/rearranging-change';
 import { BatchChange } from 'app/utils/history/change';
 import { getReorderedAffiliations } from 'app/reducers/affiliations.handlers';
+import { UpdateObjectChange } from 'app/utils/history/update-object-change';
+import { AddObjectChange } from 'app/utils/history/add-object-change';
+import {DeleteObjectChange} from "app/utils/history/delete-object-change";
 
 export function updateAuthor(state: ManuscriptHistoryState, payload: Person): ManuscriptHistoryState {
-  // const authorIndex = state.data.present.authors.findIndex(({ id }) => id === payload.id);
-  //
-  // const newDiff: ManuscriptDiff = createDiff({
-  //   [`authors.${authorIndex}`]: state.data.present.authors[authorIndex],
-  //   affiliations: state.data.present.affiliations
-  // });
-  //
-  // const newManuscript = cloneManuscript(state.data.present);
-  // newManuscript.authors[authorIndex] = payload;
-  // newManuscript.affiliations = getReorderedAffiliations(newManuscript.authors, newManuscript.affiliations);
+  const authorIndex = state.data.present.authors.findIndex(({ id }) => id === payload.id);
+
+  const authorUpdateChange = new UpdateObjectChange(
+    `authors.${authorIndex}`,
+    state.data.present.authors[authorIndex],
+    payload
+  );
+
+  const updatedManuscript = authorUpdateChange.applyChange(state.data.present);
+  const affiliationsReorderChange = getReorderedAffiliations(updatedManuscript.authors, updatedManuscript.affiliations);
 
   return {
-    ...state
-    // data: {
-    //   past: [...state.data.past, newDiff],
-    //   present: newManuscript,
-    //   future: []
-    // }
+    ...state,
+    data: {
+      past: [...state.data.past, new BatchChange([authorUpdateChange, affiliationsReorderChange])],
+      present: affiliationsReorderChange.applyChange(updatedManuscript),
+      future: []
+    }
   };
 }
 
 export function addAuthor(state: ManuscriptHistoryState, payload: Person): ManuscriptHistoryState {
-  // const newDiff: ManuscriptDiff = createDiff({
-  //   authors: state.data.present.authors,
-  //   affiliations: state.data.present.affiliations
-  // });
-  //
-  // const newManuscript = cloneManuscript(state.data.present);
-  // newManuscript.authors.push(payload);
-  // newManuscript.affiliations = getReorderedAffiliations(newManuscript.authors, newManuscript.affiliations);
-  //
+  const addAuthorChange = new AddObjectChange('authors', payload, 'id');
+
+  const updatedManuscript = addAuthorChange.applyChange(state.data.present);
+  const affiliationsReorderChange = getReorderedAffiliations(updatedManuscript.authors, updatedManuscript.affiliations);
 
   return {
-    ...state
-    //   data: {
-    //     past: [...state.data.past, newDiff],
-    //     present: newManuscript,
-    //     future: []
-    //   }
+    ...state,
+    data: {
+      past: [...state.data.past, new BatchChange([addAuthorChange, affiliationsReorderChange])],
+      present: affiliationsReorderChange.applyChange(updatedManuscript),
+      future: []
+    }
   };
 }
 
 export function moveAuthor(state: ManuscriptHistoryState, payload: MoveAuthorPayload): ManuscriptHistoryState {
   const { index, author } = payload;
   const currentIndex = state.data.present.authors.findIndex(({ id }) => id === author.id);
-  const changeAuthors = RearrangingChange.createFromItemMoved(
+  const rearrangeAuthors = RearrangingChange.createFromItemMoved(
     'authors',
     currentIndex,
     index,
     state.data.present.authors
   );
 
-  // const newManuscript = changeAuthors.applyChange(state.data.present);
-  // const newAffiliations = getReorderedAffiliations(newManuscript.authors, newManuscript.affiliations);
-  //
-  // const changeAffiliations = RearrangingChange.createFromListRearrange(
-  //   'affiliations',
-  //   state.data.present.affiliations,
-  //   newAffiliations
-  // );
+  const updatedManuscript = rearrangeAuthors.applyChange(state.data.present);
+  const affiliationsReorderChange = getReorderedAffiliations(updatedManuscript.authors, updatedManuscript.affiliations);
 
-  const change = new BatchChange([changeAuthors /*, changeAffiliations*/]);
+  const change = new BatchChange([rearrangeAuthors, affiliationsReorderChange]);
   return {
     ...state,
     data: {
       past: [...state.data.past, change],
-      present: change.applyChange(state.data.present),
+      present: affiliationsReorderChange.applyChange(updatedManuscript),
       future: []
     }
   };
 }
 
 export function deleteAuthor(state: ManuscriptHistoryState, payload: Person): ManuscriptHistoryState {
-  // const currentIndex = state.data.present.authors.findIndex(({ id }) => id === payload.id);
-  //
-  // const newDiff: ManuscriptDiff = createDiff({
-  //   authors: state.data.present.authors,
-  //   affiliations: state.data.present.affiliations
-  // });
-  //
-  // const newManuscript = cloneManuscript(state.data.present);
-  // newManuscript.authors.splice(currentIndex, 1);
-  // newManuscript.affiliations = getReorderedAffiliations(newManuscript.authors, newManuscript.affiliations);
+  const deleteAuthorChange = new DeleteObjectChange('authors', payload, 'id');
+
+  const updatedManuscript = deleteAuthorChange.applyChange(state.data.present);
+  const affiliationsReorderChange = getReorderedAffiliations(updatedManuscript.authors, updatedManuscript.affiliations);
 
   return {
-    ...state
-    // data: {
-    //   past: [...state.data.past, newDiff],
-    //   present: newManuscript,
-    //   future: []
-    // }
+    ...state,
+    data: {
+      past: [...state.data.past, new BatchChange([deleteAuthorChange, affiliationsReorderChange])],
+      present: affiliationsReorderChange.applyChange(updatedManuscript),
+      future: []
+    }
   };
 }
