0f3b32848b9150966e7233c889e2baf1f99ba58e
Will Byrne <w.byrne@elifesciences.org>
2021-11-17 17:43:26 +0000

    feat: keywords tests - add tests and browser objects (libero/editor#938) (#223)
    * feat: keywords tests - add tests and browser objects (libero/editor#938)
    
    * feat: keywords tests - fix and rename accidentally unused parameter (libero/editor#938)
    
    closes libero/editor#938

---
+++ tests/keywords.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '@playwright/test';
+import { Keywords } from './page-objects/keywords';
+
+test.describe('keywords', () => {
+  let keywords: Keywords
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/?articleId=54296');
+    keywords = new Keywords(page);
+  });
+
+  test('set first keywords field', async ({ page }) => {
+    const testWords = ['Red Dwarf', 'Starbug', 'Blue Midget'];
+    await keywords.addKeywords(testWords, 0);
+    await keywords.assertKeywords(testWords, 0);
+  });
+
+  test('set second keywords field', async ({ page }) => {
+    const testWords = ['Gelf', 'Emohawk', 'Siren', 'Simulant'];
+    await keywords.addKeywords(testWords, 1);
+    await keywords.assertKeywords(testWords, 1);
+  });
+
+  test('delete keywords', async ({ page }) => {
+    await keywords.deleteKeyword(0, 0);
+  });
+});
---
+++ tests/page-objects/keywords.ts
@@ -0,0 +1,32 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class Keywords {
+  private readonly page: Page;
+  private readonly keywords: Locator;
+
+  constructor(thePage: Page) {
+    this.page = thePage;
+    this.keywords = this.page.locator('section:below(label:has-text("Keywords"))');
+  }
+
+  async addKeywords(keywords: string[], keywordsNumber: 0 | 1): Promise<void> {
+    for(let i = 0; i < keywords.length; i++) {
+      await this.keywords.nth(keywordsNumber).locator('div[contenteditable=true]').last().type(keywords[i]);
+      await this.keywords.nth(keywordsNumber).locator('div[contenteditable=true]').last().press('Enter');
+    }
+  }
+
+  async assertKeywords(keywords: string[], keywordsNumber: number): Promise<void> {
+    for(let i = 0; i < keywords.length; i++) {
+      await expect(this.keywords.nth(keywordsNumber)).toContainText(keywords[i]);
+    }
+  }
+
+  async deleteKeyword(keywordToDelete: number, keywordsNumber: 0 | 1): Promise<void> {
+    const keywords = this.keywords.nth(keywordsNumber);
+    const keywordsContainer = keywords.locator('[data-test-id="keyword-container"]');
+    const keywordCount = await keywordsContainer.count();
+    await keywordsContainer.nth(keywordToDelete).locator('button').click();
+    expect(await keywordsContainer.count()).toBeLessThan(keywordCount);
+  }
+}
