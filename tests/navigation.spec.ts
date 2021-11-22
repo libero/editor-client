314e994cc7c8d36be1f77aefab8eb7032c927a73
Will Byrne <w.byrne@elifesciences.org>
2021-11-22 17:53:12 +0000

    feat: navigation tests - add page objects and tests (libero/editor#939)

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,106 @@
+import { test, Page } from '@playwright/test';
+import { MainText } from './page-objects/main-text';
+import { Navigation } from './page-objects/navigation';
+
+const content = `
+    To Ganymede and Titan
+    Yes, sir, I've been around
+    But there ain't no place
+    In the whole of Space
+    Like that good ol' toddlin' town
+    Oh! Lunar City Seven
+    You're my idea of heaven
+    Out of ten, you score eleven
+    You good ol' Titan' town
+    Oh! Lunar City Seven
+    Lunar Cities One through Six
+    They always get me down
+    But Lunar City Seven
+    You're my home town
+  `;
+
+const setHeadingAndTestNavigation = async (page: Page, navigation: Navigation, heading: string): Promise<void> => {
+  const mainText = new MainText(page);
+  await mainText.setText(content);
+  const text = 'To Ganymede and Titan';
+  await mainText.selectText(text);
+  await mainText.setTextHeading(heading);
+  await navigation.clickLink(text);
+  await navigation.assertTargetInViewport(page.locator(`#mainText>div>div`).locator('text="To Ganymede and Titan"'));
+}
+
+test.describe('navigation', () => {
+  let navigation: Navigation;
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/?articleId=54296');
+    navigation = new Navigation(page);
+  });
+
+  test('title', async ({ page }) => {
+    await navigation.clickLink('Title');
+    await navigation.assertTargetInViewport(page.locator('#title'));
+  });
+
+  test('authors', async ({ page }) => {
+    await navigation.clickLink('Authors');
+    await navigation.assertTargetInViewport(page.locator('#authors'));
+  });
+
+  test('affiliations', async ({ page }) => {
+    await navigation.clickLink('Affiliations');
+    await navigation.assertTargetInViewport(page.locator('#affiliations'));
+  });
+
+  test('abstract', async ({ page }) => {
+    await navigation.clickLink('Abstract');
+    await navigation.assertTargetInViewport(page.locator('#abstract'));
+  });
+
+  test('impact statement', async ({ page }) => {
+    await navigation.clickLink('Impact statement');
+    await navigation.assertTargetInViewport(page.locator('#impactStatement'));
+  });
+
+  test('acknowledgements', async ({ page }) => {
+    await navigation.clickLink('Acknowledgements');
+    await navigation.assertTargetInViewport(page.locator('#acknowledgements'));
+  });
+
+  test('references', async ({ page }) => {
+    await navigation.clickLink('References');
+    await navigation.assertTargetInViewport(page.locator('#references'));
+  });
+
+  test('author information', async ({ page }) => {
+    await navigation.clickLink('Author information');
+    await navigation.assertTargetInViewport(page.locator('#author-details'));
+  });
+
+  test('article info', async ({ page }) => {
+    await navigation.clickLink('Article information');
+    await navigation.assertTargetInViewport(page.locator('#article-info'));
+  });
+
+  test('related articles', async ({ page }) => {
+    await navigation.clickLink('Related articles');
+    await navigation.assertTargetInViewport(page.locator('#realted-acticles'));
+  });
+
+  test.describe('dynamic main text headings', () => {
+    test('heading 1', async ({ page }) => {
+      await setHeadingAndTestNavigation(page, navigation, 'Heading 1');
+    });
+
+    test('heading 2', async ({ page }) => {
+      await setHeadingAndTestNavigation(page, navigation, 'Heading 2');
+    });
+
+    test('heading 3', async ({ page }) => {
+      await setHeadingAndTestNavigation(page, navigation, 'Heading 3');
+    });
+
+    test('heading 4', async ({ page }) => {
+      await setHeadingAndTestNavigation(page, navigation, 'Heading 4');
+    });
+  });
+});
