53792f95b11a8992d509d0917467b7fb6cd6fd04
Will Byrne <w.byrne@elifesciences.org>
2021-12-06 18:45:23 +0000

    feat: related article tests - add tests and page object (libero/editor#943) (#227)

---
+++ tests/related-articles.spec.ts
@@ -0,0 +1,24 @@
+import { test } from '@playwright/test';
+import { ArticleType, RelatedArticles } from './page-objects/related-articles';
+
+test.describe.only('related articles', () => {
+  let relatedArticles: RelatedArticles;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/?articleId=54296');
+    relatedArticles = new RelatedArticles(page);
+    await page.waitForSelector('#title');
+  });
+
+  test('add a related article', async () => {
+    await relatedArticles.addRelatedArticle('Salt: An epicure\'s delight', ArticleType.COMMENTARYARTICLE);
+  });
+
+  test('remove a related article', async () => {
+    await relatedArticles.removeRelatedArticle(0);
+  });
+
+  test('edit a related article', async () => {
+    await relatedArticles.editRelatedArticle(0, 'Salt: An epicure\'s delight', ArticleType.CORRECTEDARTICLE);
+  });
+});
