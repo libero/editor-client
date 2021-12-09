711f3330782aa9ed41764cf618a67b4ee3a29055
Will Byrne <w.byrne@elifesciences.org>
2021-12-09 16:16:43 +0000

    feat: test cleanup - harmonize use of page objects (libero/editor#944)

--- tests/affiliations.spec.ts
+++ tests/affiliations.spec.ts
@@ -8,13 +8,15 @@ const affiliation = {
 };
 
 test.describe('affiliations', () => {
+  let affiliations: Affiliations;
+
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000/?articleId=54296');
     await page.waitForSelector('#title');
+    affiliations = new Affiliations(page);
   });
 
-  test('affiliations are set to correct values', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('affiliations are set to correct values', async () => {
     await affiliations.assertAffiliation({
       institution: 'Biomedical and Biological Sciences Program, Cornell University',
       city: 'Ithaca',
@@ -22,43 +24,36 @@ test.describe('affiliations', () => {
     }, 0);
   });
 
-  test('add an affiliation', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('add an affiliation', async () => {
     await affiliations.addAffiliation(affiliation);
     await affiliations.assertAffiliation(affiliation, 2);
   })
 
-  test('edit institution', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('edit institution', async () => {
     await affiliations.setInstitution(affiliation.institution, 0);
     await affiliations.assertInstitution(affiliation.institution, 0);
   });
 
-  test('edit city', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('edit city', async () => {
     await affiliations.setCity(affiliation.city, 0);
     await affiliations.assertCity(affiliation.city, 0);
   })
 
-  test('edit country', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('edit country', async () => {
     await affiliations.setCountry(affiliation.country, 0);
     await affiliations.assertCountry(affiliation.country, 0);
   })
 
-  test('add an author', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('add an author', async () => {
     await affiliations.addAuthor(0, 'Cheng-Lin Li');
     await affiliations.assertAuthorHasAffiliations(0, [1,2]);
   })
 
-  test('delete an author', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('delete an author', async () => {
     await affiliations.deleteAuthor(1, 0);
   })
 
-  test('delete an affiliation', async ({ page }) => {
-    const affiliations = new Affiliations(page);
+  test('delete an affiliation', async () => {
     await affiliations.deleteAffiliation(0);
   })
 });
